a4fc070e3455b4cd55cbcbc214ff189d234887a3
nanagami1369 <nanagami1369@gmail.com>
2022-08-11 23:46:08 +0900

    Release用のビルドスクリプトを書いた

---
+++ buildTools/Packaging.ps1
@@ -0,0 +1,7 @@
+﻿$workingDirectory = $Args[0]
+# 圧縮
+Copy-Item -Recurse $workingDirectory $workingDirectory\..\AtTheFront
+7z a $workingDirectory\..\AtTheFront.zip $workingDirectory
+# ハッシュ生成
+(Get-FileHash -Algorithm SHA512 -Path $workingDirectory\..\AtTheFront.zip).Hash > "$workingDirectory\..\AtTheFront_zip_sha512.hash"
+Remove-Item -Recurse $workingDirectory\..\AtTheFront
