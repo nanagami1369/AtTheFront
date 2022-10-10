0bd3ccc36d87bcc2ad8f5933fe3729c26c5830c5
nanagami1369 <nanagami1369@gmail.com>
2022-10-10 11:49:23 +0900

    リリースビルド時にTargetFrameworkの名称が生成されたフォルダに入ってしまっていた問題を修正

--- buildTools/Packaging.ps1
+++ buildTools/Packaging.ps1
@@ -1,7 +1,7 @@
 ﻿$workingDirectory = $Args[0]
 # 圧縮
-Copy-Item -Recurse $workingDirectory $workingDirectory\..\AtTheFront
-7z a $workingDirectory\..\AtTheFront.zip $workingDirectory
+Copy-Item -Recurse $workingDirectory "$workingDirectory\..\AtTheFront"
+7z a $workingDirectory\..\AtTheFront.zip "$workingDirectory\..\AtTheFront"
 # ハッシュ生成
 (Get-FileHash -Algorithm SHA512 -Path $workingDirectory\..\AtTheFront.zip).Hash > "$workingDirectory\..\AtTheFront_zip_sha512.hash"
-Remove-Item -Recurse $workingDirectory\..\AtTheFront
+Remove-Item -Recurse "$workingDirectory\..\AtTheFront"
